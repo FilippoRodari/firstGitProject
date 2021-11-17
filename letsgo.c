09d277181a443cfc98fbf7c05b9f3940786a26bb
FilippoRodari <filippo.rodari@gmail.com>
2021-11-17 10:57:33 +0100

    Added comment line 4 from rep2

--- letsgo.c
+++ letsgo.c
@@ -1,7 +1,8 @@
 /* Added comment from repository 1 (machine 1) */
 #include <stdio.h>
 
-int main(){
+int main()//Another comment from repository 2 (machine 2)
+{
 printf("Git,let's go!!\n");
 printf("added line 1!\n");
 
